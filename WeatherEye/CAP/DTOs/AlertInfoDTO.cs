16a4e41e02c27b725a2bb9ba57890d8025c1b0ea
TenCerak <cerovsky2@gmail.com>
2025-10-28 17:21:14 +0100

    Pridani DTO pro lepší komunikaci mezi službami

---
+++ WeatherEye/CAP/DTOs/AlertInfoDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAP.DTOs
+{
+    public class AlertInfoDTO
+    {
+        public string SenderName { get; set; } = string.Empty;
+        public string Event { get; set; } = string.Empty;
+        public string Urgency { get; set; } = string.Empty;
+        public string Severity { get; set; } = string.Empty;
+        public string Certainty { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+        public DateTime Onset { get; set; }
+        public DateTime Expires { get; set; }
+        public string Headline { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Instruction { get; set; } = string.Empty;
+        
+        public string AreaDesc { get; set; } = string.Empty;
+
+
+
+
+        public AlertInfoDTO()
+        {
+
+        }      
+
+    }
+}
