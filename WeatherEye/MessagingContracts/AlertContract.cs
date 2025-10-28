07bee2bfe2d2f47e0f53b013ade336c035b306e0
TenCerak <cerovsky2@gmail.com>
2025-10-28 17:22:33 +0100

    přidání knihovny na messaging kontrakty

---
+++ WeatherEye/MessagingContracts/AlertContract.cs
@@ -0,0 +1,14 @@
+﻿using CAP;
+using CAP.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagingContracts
+{
+    public record AlertRequest;
+    public record AlertResponse(IEnumerable<AlertInfoDTO> records);
+    
+}
