d945b3a0bc1e5b5eff55f41d55acd6abaa6a66a3
TenCerak <cerovsky2@gmail.com>
2025-11-22 16:35:48 +0100

    Rozšíření kontraktů pro komunikaci

--- WeatherEye/MessagingContracts/UserContracts.cs
+++ WeatherEye/MessagingContracts/UserContracts.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Preferences.DTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MessagingContracts
 {
-    public record SaveAlertPreferenceRequest(Guid UserOid, string AreaDesc);
+    public record SaveAlertPreferenceRequest(Guid UserOid, string AreaDesc, bool EmailNotification, bool InAppNotification);
     public record SaveAlertPreferenceResponse(Guid PreferenceOid);
 
+    public record RemoveAlertPreferenceRequest(Guid UserOid,Guid PreferenceOid);
+    public record RemoveAlertPreferenceResponse(Guid PreferenceOid);
+
     public record AlertPreferencesRequest(Guid UserOid);
-    public record AlertPreferencesResponse(IEnumerable<string> AreaDescs);
+    public record AlertPreferencesResponse(IList<AlertPreferenceDTO> AlertPreferences);
 
     public record UserOidRequest(string Username,string Email);
     public record UserOidResponse(Guid UserOid);
+
+    public record SaveUserMobAppIdRequest(Guid UserOid,string MobAppId);
+    public record SaveUserMobAppIdResponse(bool ok);
 }
