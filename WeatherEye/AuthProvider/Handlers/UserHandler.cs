3464f215649f30a5ffd90f34d6ff2ce4044c437a
TenCerak <cerovsky2@gmail.com>
2025-11-23 19:21:51 +0100

    opravy práce se kontrakty a jejich rozšíření

--- WeatherEye/AuthProvider/Handlers/UserHandler.cs
+++ WeatherEye/AuthProvider/Handlers/UserHandler.cs
@@ -68,5 +68,26 @@ namespace UserService.Handlers
             _logger.LogInformation("User MobAppId updated for UserOid: {UserOid}, MobAppId: {MobAppId}", request.UserOid, request.MobAppId);
             return new(true);
         }
+
+        [WolverineHandler]
+        public InterestedUserEmailsResponse InterestedUserEmailsRequestHandler(InterestedUserEmailsRequest request)
+        {
+            _logger.LogInformation("InterestedUserEmailsRequestHandler called for AlertInfo: Event - {event}, AreaDesc - {areaDesc}", request.AlertInfo.Event, request.AlertInfo.AreaDesc);
+
+            XPQuery<UserAlertPreferenceDAO> preferenceDAOs = Session.DefaultSession.Query<UserAlertPreferenceDAO>();
+
+            var userEmails = preferenceDAOs
+                .Where(p =>
+                    p.AreaDesc == request.AlertInfo.AreaDesc &&
+                    
+                    p.EmailNotification == true
+                )
+                .Select(p => p.User.Email)
+                .Distinct()
+                .ToList();
+
+            _logger.LogInformation("Found {Count} interested user emails for AlertInfo: Event - {event}, AreaDesc - {areaDesc}", userEmails.Count, request.AlertInfo.Event, request.AlertInfo.AreaDesc);
+            return new InterestedUserEmailsResponse(userEmails);
+        }
     }
 }
